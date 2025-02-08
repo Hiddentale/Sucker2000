eb9407f7ff30191f89c0b5a9eb4714a07e5c4b8a
Seloria <55276630+Hiddentale@users.noreply.github.com>
2025-02-08 19:06:07 +0100

    Create main.rs

---
+++ main.rs
@@ -0,0 +1,37 @@
+use serde_json;
+use std::env;
+
+
+enum BencodeError {
+    EmptyInput,
+    MissingColon,
+    InvalidLength,
+    InvalidFormat
+}
+
+
+fn find_colon(encoded_value: &str) {
+
+}
+
+#[allow(dead_code)]
+fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
+    if encoded_value.is_empty() {
+        return Err(BencodeError::EmptyInput);
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let command = &args[1];
+
+    if command == "decode" {
+        eprintln!("Logs");
+
+        let encoded_value = &args[2];
+        let decoded_value = decode_bencoded_value(encoded_value);
+        println!("{}", decoded_value.to_string());
+    } else {
+        println!("unknown command: {}", args[1])
+    }
+}
