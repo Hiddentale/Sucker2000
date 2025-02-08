27359264c86918054df44363a799ccef59db21ad
Seloria <55276630+Hiddentale@users.noreply.github.com>
2025-02-08 21:55:20 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ enum BencodeError {
     InvalidFormat
 }
 
-fn decode_string(encoded_value: &str) -> Return(serde_json::Value, BencodeError) {
+fn decode_string(encoded_value: &str) -> Result<serde_json::Value, BencodeError> {
     let colon_index = encoded_value.find(':').ok_or(BencodeError::MissingColon)?;
     let number = encoded_value[..colon_index].parse::<i64>().map_err(|_| BencodeError::InvalidLength)?;
     let end_index = colon_index + 1 + number as usize;
@@ -20,19 +20,26 @@ fn decode_string(encoded_value: &str) -> Return(serde_json::Value, BencodeError)
     return Ok(serde_json::Value::String(string.to_string()));
 }
 
-fn decode_integer() {}
-fn decode_list() {}
-fn decode_dictionary() {}
+fn decode_integer(encoded_value: &str) -> Result<serde_json::Value, BencodeError> {
+    if encoded_value.chars().last() != 'e' {
+        return Err(BencodeError::InvalidFormat);
+    }
+    let integer = &encoded_value[1..encoded_value.len() - 1].parse::<i64>().map_err(|_| BencodeError::InvalidLength)?;
+    return Ok(serde_json::Value::Number(integer));
+}
 
-fn decode_bencoded_value(encoded_value: &str) -> Return(serde_json::Value, BencodeError) {
+fn decode_list(encoded_value: &str) -> Result<serde_json::Value, BencodeError> {}
+fn decode_dictionary(encoded_value: &str) -> Result<serde_json::Value, BencodeError> {}
+
+fn decode_bencoded_value(encoded_value: &str) -> Result<serde_json::Value, BencodeError> {
     if encoded_value.is_empty() {
         return Err(BencodeError::EmptyInput);
     }
     match encoded_value.chars().next().unwrap() {
-        '0'..='9' => decode_string(),
-        'i' => decode_integer(),
-        'l' => decode_list(),
-        'd' => decode_dictionary()
+        '0'..='9' => decode_string(encoded_value),
+        'i' => decode_integer(encoded_value),
+        'l' => decode_list(encoded_value),
+        'd' => decode_dictionary(encoded_value)
         invalid_character => {
             eprintln("Invalid initial character: {}", invalid_character)
             Err(BencodeError::InvalidFormat);
